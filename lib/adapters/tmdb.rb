b109383b3adc94e5b422388ab2a6b8ac7afef971
Matheus Lopes <matheuslopes.developer@gmail.com>
2019-12-12 21:44:04 -0300

    Upcoming movies request

--- lib/adapters/tmdb.rb
+++ lib/adapters/tmdb.rb
@@ -7,17 +7,17 @@ class Tmdb
   # but a have no time for this right now !
   TOKEN = 'c5850ed73901b8d268d0898a8a9d8bff'
 
-  BASE_URL = 'https://api.themoviedb.org/3'
+  BASE_URL = 'https://api.themoviedb.org'
 
   API_VERSION = '3'
 
   def upcoming_movies
-    response = self.class.get("#{BASE_URL}/#{API_VERSION}/movie/upcoming=?api_key#{TOKEN}")
+    response = self.class.get("#{BASE_URL}/#{API_VERSION}/movie/upcoming?api_key=#{TOKEN}")
     response.parsed_response
   end
 
-  def show_movie
-    response = self.class.get("#{BASE_URL}/#{API_VERSION}/movie/76341=?api_key#{TOKEN}")
+  def show_movie(movie_id)
+    response = self.class.get("#{BASE_URL}/#{API_VERSION}/movie/#{movie_id}=?api_key#{TOKEN}")
     response.parsed_response
   end
 
