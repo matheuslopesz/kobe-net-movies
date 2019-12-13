05572d11491d61b262f54eac4412e413757562bb
Matheus Lopes <matheuslopes.developer@gmail.com>
2019-12-12 22:23:07 -0300

    Home index has now the main info to build the movie

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_action :new_tmdb, only: [:index]
+
   def index
+    @upcoming_movies = @tmdb.upcoming_movies
+  end
+
+  def new_tmdb
+    @tmdb = Tmdb.new
   end
 end
