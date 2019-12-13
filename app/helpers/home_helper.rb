da8f99ccff6d3918980d84868f7adff85f125753
Matheus Lopes <matheuslopes.developer@gmail.com>
2019-12-12 22:52:56 -0300

    Poster, release date, and title now on index home

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def format_release_date(date)
+    l date.to_date, format: :long 
+  end
+
+  def format_genres(genres)
+    only_title = []
+    genres.each do |genre|
+      #onyle_title << genre[]
+    end
+  end
 end
