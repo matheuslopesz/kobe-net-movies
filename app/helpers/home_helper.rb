c4f4dea081ffded87bb3597c033b66b6773d2eac
Matheus Lopes <matheuslopes.developer@gmail.com>
2019-12-13 19:04:47 -0300

    Refactoring Helpers

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
 module HomeHelper
-  def format_release_date(date)
-  rescue StandardError
-    l date.to_date, format: :long
-  end
-
-  def color_average(average)
-    if average.zero?
-      'no-score'
-    elsif average.between?(7.0, 7.9)
-      'average-score'
-    elsif average > 7.0
-      'good-score'
-    elsif average <= 6.9
-      'bad-score'
-    end
-  end
+  
 end
--- app/helpers/movie_helper.rb
+++ app/helpers/movie_helper.rb
@@ -2,14 +2,33 @@
 
 module MovieHelper
   def format_runtime(runtime)
-  rescue StandardError
+    return if runtime.nil?
+
     hours = runtime / 60
     rest = runtime % 60
     "#{hours}h #{rest}min"
   end
 
   def format_average(average)
+    return if average.nil?
+
     'not rated yet' if average.zero?
     average
+  end
+
+  def format_release_date(date)
+    date.nil? ? l(date.to_date, format: :long) : 'no date'
+  end
+
+  def color_average(average)
+    if average.zero?
+      'no-score'
+    elsif average.between?(7.0, 7.9)
+      'average-score'
+    elsif average > 7.0
+      'good-score'
+    elsif average <= 6.9
+      'bad-score'
+    end
   end
 end
